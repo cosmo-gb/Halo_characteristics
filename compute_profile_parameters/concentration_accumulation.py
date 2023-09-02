c260ea8653e648dbd86bd5ff00d1e96001bca71a
cosmo-gb <55383209+cosmo-gb@users.noreply.github.com>
2023-09-02 19:10:05 +0200

    check results for c for sa halo

--- compute_profile_parameters/concentration_accumulation.py
+++ compute_profile_parameters/concentration_accumulation.py
@@ -29,7 +29,7 @@ from semi_analytical_halos.generate_smooth_halo import Smooth_halo
 
 class Concentration_accumulation():
 
-    def NFW_function(self, x: np.ndarray[float]) -> np.ndarray[float]:
+    def NFW_function(self, x: np.ndarray[float],) -> np.ndarray[float]:
         """
         This computes the NFW function y = ln(1+x) - x/(1+x)
         #############################################################################
@@ -41,7 +41,7 @@ class Concentration_accumulation():
         """
         return np.log(1+x) - x/(1+x)
 
-    def mass_in_r_s_NFW(self, Mass_total: float, r_s: np.ndarray[float]) -> np.ndarray[float]:
+    def mass_in_r_s_NFW(self, Mass_total: float, r_s: np.ndarray[float],) -> np.ndarray[float]:
         """
         This function computes the mass inside r_s of an NFW profile.
         It is using the equation 6 of 
@@ -58,7 +58,14 @@ class Concentration_accumulation():
         """
         return Mass_total * (np.log(2) - 0.5)/self.NFW_function(1/r_s)
         
-    def compute_c_NFW_acc(self, r_data: np.ndarray[float], do_plot: bool=False) -> Tuple[float, bool]:
+
+    def smooth(self, y: np.ndarray[float], box_pts: int,) -> np.ndarray[float]:
+        box = np.ones(box_pts)/box_pts
+        y_smooth = np.convolve(y, box, mode='same')
+        return y_smooth
+
+    def compute_c_NFW_acc(self, r_data: np.ndarray[float], box_pts: int=30, 
+                          do_plot: bool=False,) -> Tuple[float, bool]:
         """
         This function computes the concentration of a halo with the accumulated mass technique.
         It follows the equation 6 of https://iopscience.iop.org/article/10.3847/1538-4357/aabf95/pdf
@@ -66,6 +73,7 @@ class Concentration_accumulation():
         #############################################################################
         Input parameters:
         - r_data: np.array of float, radius of particles in the halo
+        - box_pts: int, default = 30, for smoothing for convolution
         - do_plot: bool, default is False, plot or not the masses M(<r)
         #############################################################################
         Returns:
@@ -80,6 +88,7 @@ class Concentration_accumulation():
         M_in_sphere_data = range(1, N_end+1) # cumulative mass
         # then I compute the mass within r_s, assuming that r=r_s for all r
         M_in_sphere_r_s_NFW = self.mass_in_r_s_NFW(N_part_tot, r_data_sorted[0:N_end])
+        M_in_sphere_r_s_NFW = self.smooth(M_in_sphere_r_s_NFW, box_pts)
         # difference between the masses: M(<r) - M_NFW(<r_s=r)
         diff = M_in_sphere_data - M_in_sphere_r_s_NFW 
         # the mass difference changes of sign at r=r_s
@@ -91,6 +100,8 @@ class Concentration_accumulation():
             #r_s = r_data_sorted[ind_sign_change[0]]
             #r_s = r_data_sorted[ind_sign_change[0]+1]
             r_s = r_data_sorted[ind_sign_change[0]-1]
+            # I do not know why but it works better for N=1000
+            #r_s = r_data_sorted[ind_sign_change[0]-7] 
         else: # case with at least 2 solutions
             ok = False
             r_s = np.sum(r_data_sorted[ind_sign_change])/len(ind_sign_change)
@@ -111,20 +122,20 @@ if __name__ == '__main__':
     kind_profile = {"kind of profile": "abg",
                     "concentration": 10,
                     "alpha": 1, "beta": 3, "gamma": 1}
-    my_halo = halo.smooth_halo_creation() #kind_profile, b_ax=0.5, c_ax=0.5)
-    data = my_halo["data"]
-    print("N_tot =",my_halo["N_tot"])
+    #my_halo = halo.smooth_halo_creation() #kind_profile, b_ax=0.5, c_ax=0.5)
+    #data = my_halo["data"]
+    #print("N_tot =",my_halo["N_tot"])
     #halo.plot_data(data[:,0],data[:,1])
-    halo.beauty_plot_colorbar(data)
+    #halo.beauty_plot_colorbar(data)
 
-    r_data = np.sqrt(data[:,0]**2 + data[:,1]**2 + data[:,2]**2)
+    #r_data = np.sqrt(data[:,0]**2 + data[:,1]**2 + data[:,2]**2)
     c_acc = Concentration_accumulation()
-    conc, ok = c_acc.compute_c_NFW_acc(r_data)
-    print(conc)
-    N_times = 1000
+    #conc, ok = c_acc.compute_c_NFW_acc(r_data)
+    #print(conc)
+    N_times = 100
     c_arr = np.zeros((N_times))
     for t in range(N_times):
-        my_halo = halo.smooth_halo_creation(N_part=3000) #kind_profile, b_ax=0.5, c_ax=0.5)
+        my_halo = halo.smooth_halo_creation(N_part=100000) #kind_profile, b_ax=0.5, c_ax=0.5)
         data = my_halo["data"]
         r_data = np.sqrt(data[:,0]**2 + data[:,1]**2 + data[:,2]**2)
         c_arr[t], ok = c_acc.compute_c_NFW_acc(r_data)
