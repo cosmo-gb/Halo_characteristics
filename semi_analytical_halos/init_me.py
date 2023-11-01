e568acee52ba4cca9f4011e18ba509282a101fa8
cosmo-gb <guillaume5.bonnet@gmail.com>
2023-11-01 14:27:57 +0100

    remove useless init

--- semi_analytical_halos/init_me.py
+++
@@ -1,6 +0,0 @@
-"""
-from .generate_smooth_halo import generate_smooth_halo
-from .compute_tidal_radii import compute_tidal_radii
-from .density_profile import density_profile
-from .generate_halo_with_subhalos import generate_halo_with_subhalos
-"""
